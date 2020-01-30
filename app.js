c9ff8d66e455b55416c3a0de3ca51031364c7923
cprom <chen.prom@gmail.com>
2020-01-29 21:55:24 -0800

    create render function

--- app.js
+++ app.js
@@ -1,35 +1,46 @@
+let start = document.getElementById("start")
+let question = document.getElementById("question")
+let answerA = document.getElementById("answerA")
+let answerB = document.getElementById("answerB")
+let answerC = document.getElementById("answerC")
+let quiz = document.getElementById("quiz")
+
 //Build array of objects for questions and answers
 
 let qAndA = [
   {
     question: "Name one data type in JS",
-    a: "String",
-    b: "Sections",
-    c: "not hotdog",
-    d: "block",
+    answerA: "String",
+    answerB: "Sections",
+    answerC: "not hotdog",
     correctAnswer: 'a'
   },
 
   {
     question: "What is used to declare a variable?",
-    a: "prinf",
-    b: "let",
-    c: "<h1>",
-    d: "css",
-    CorrectAnswer: 'b'
+    answerA: "prinf",
+    answerB: "let",
+    answerC: "<h1>",
+    correctAnswer: 'b'
   },
 
 ]
 
 let lastQuestion = qAndA.length - 1
-let arrQuestion = 0;
+let nextQuestion = 0;
 
 function renderQuestion() {
-  let q = question[arrQuestion]
+  let q = qAndA[nextQuestion]
   question.innerHTML = "<p>" + q.question + "</p>"
-  a.innerHTML = q.a
-  b.innerHTML = q.b
-  b.innerHTML = q.c
-  b.innerHTML = q.d
+  answerA.innerHTML = q.answerA
+  answerB.innerHTML = q.answerB
+  answerC.innerHTML = q.answerC
+  nextQuestion = 0
+  nextQuestion++
+
+
 }
 
+start.style.display = "none"
+renderQuestion()
+quiz.style.display = "block"
